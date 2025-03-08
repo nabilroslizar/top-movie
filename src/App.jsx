6bd64c151336d4e5633d569a1322e5d67c9736ee
Nabil <nabil.roslizar@gmail.com>
2025-03-08 12:53:11 +1100

    card component image

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,55 @@
+import { useState, useEffect } from "react";
+import Search from "./components/Search.jsx";
+import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
 
-import { useState, useEffect } from 'react'
-import Search from './components/Search.jsx'
-import Spinner from './components/Spinner.jsx'
-
-const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_BASE_URL = "https://api.themoviedb.org/3";
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTIONS = {
-  method: 'GET',
-  headers:{
-    accept: 'application/json',
-    Authorization: `Bearer ${API_KEY}`
-  }
-}
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const[errorMessage, setErrorMessage] = useState();
-  const [movieList, setMovieList] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState();
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () =>{
+  const fetchMovies = async () => {
     setIsLoading(true);
-    setErrorMessage('');
+    setErrorMessage("");
 
-    try{
+    try {
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
 
-      if(!response.ok){
-        throw new Error('Failed to fetch movies');
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
       }
 
       const data = await response.json();
 
-      if (data.Response === 'False'){
-        setErrorMessage(data.Error || 'Failed to fetch movies');
+      if (data.Response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies");
         setMovieList([]);
         return;
       }
 
       setMovieList(data.results || []);
-
-    } catch(error){
+    } catch (error) {
       console.error(`Error fetching movies: ${error}`);
-      setErrorMessage('Error fetching movies. Please try again later.');
+      setErrorMessage("Error fetching movies. Please try again later.");
     } finally {
       setIsLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     fetchMovies();
@@ -58,37 +57,39 @@ const App = () => {
 
   return (
     <main>
-      <div className='pattern'/>
+      <div className="pattern" />
 
-    <div className='wrapper'>
-      <header>
-      <img src="./hero.png" alt="Hero Banner"/>
-      <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
-  
-      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-      </header>
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
 
-      <section className='all-movies'>
-        <h2>All Movies</h2>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
 
-        {isLoading ? (
-          <Spinner/>
-        ) : errorMessage ? (
-          <p className='text-red-500'>{errorMessage}</p>
-        ) : (
-          <ul>
-            {movieList.map((movie)=>(
-              <p key={movie.id} className='text-white'>{movie.title}</p>
-            ))}
-          </ul>
-        )}
+        <section className="all-movies">
+          <h2 className="mt-[40px]">All Movies</h2>
 
-        {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
-      </section>
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
 
-    </div>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
